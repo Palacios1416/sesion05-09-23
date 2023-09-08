8165be9447bf1209ea09a39c22778add61c909f4
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-09-08 14:15:36 -0600

    Continuamos

--- EjerciciosEnClase/src/Ejercicio2.java
+++ EjerciciosEnClase/src/Ejercicio2.java
@@ -5,20 +5,46 @@ public class Ejercicio2 {
         Scanner scanner = new Scanner(System.in);
 
         double lado1 = 0, lado2 = 0, lado3 = 0;
+        boolean condition;
 
         System.out.println("TRIANGULO");
         System.out.println("Inserte la longitud del primer lado: ");
+        do {
+        lado1 = scanner.nextDouble();    
+        } while (lado1 <= 0);
+        
         System.out.println("Inserte la longitud del segundo lado: ");
+        do {
+        lado2 = scanner.nextDouble();    
+        } while (lado2 <= 0);
         System.out.println("Inserte la longitud del tercer lado: ");
+        do {
+        lado3 = scanner.nextDouble();    
+        } while (lado3 <= 0);
 
-        if (lado1 == lado2 && lado2 == lado3) {
+        if ((lado1 + lado2) == lado3 || (lado2 + lado3) == lado1 || (lado1 + lado3) == lado2) {
+
+            if (lado1 == lado2 && lado2 == lado3 && lado3 == lado1) {
+
             System.out.println("Es un triangulo equilatero");
-        }else if (lado1 != lado2 && lado2 != lado3) {
+
+            }else if (lado1 != lado2 && lado2 != lado3 && lado3 != lado1) {
+
             System.out.println("Es un triangulo escaleno");
-        }else if (lado1 == lado2 || lado2 == lado3 || lado3 == lado1) {
+
+            }else if (lado1 == lado2 || lado2 == lado3 || lado3 == lado1) {
+
             System.out.println("Es un triangulo isosceles");
-        } else {
-            
+
+            }  
+
+        } else if ((lado1*lado1) == (lado2*lado2) + (lado3*lado3)){
+
+            System.out.println("Es un triangulo rectangulo");
+
+            }else {
+                
+            System.out.println("No es un triangulo");
         }
     }
 }
